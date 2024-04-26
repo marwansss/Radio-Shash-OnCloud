65fa1c464be6f696dfd5703311d013f84eee410f
Marwan Abdelaziz <106702769+marwansss@users.noreply.github.com>
2024-04-26 04:15:52 +0300

    Radioshash optimizations and updates

--- Terraform/modules/s3_module/main.tf
+++ Terraform/modules/s3_module/main.tf
@@ -9,6 +9,13 @@ resource "aws_s3_bucket" "S3logs_storage" {
   }
 }
 
+#push the AI model to the s3 to be retrieve when building docker image
+resource "aws_s3_bucket_object" "example" {
+  key                    = "model_2 (3).h5"
+  bucket                 = aws_s3_bucket.S3logs_storage.id
+  source                 = "model_2 (3).h5"
+}
+
 #adding s3 policy to store lb access logs
 resource "aws_s3_bucket_policy" "lb_logs_policy" {
   bucket = aws_s3_bucket.S3logs_storage.id
